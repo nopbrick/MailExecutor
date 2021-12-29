7b73059decf015b05a5813ea087ebdbe519421c0
Filip Ceglik <fceglik@gmail.com>
2021-12-29 21:38:47 +0100

    Cleanup

--- Executor/ThisAddIn.cs
+++ Executor/ThisAddIn.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
-using Outlook = Microsoft.Office.Interop.Outlook;
-using Office = Microsoft.Office.Core;
-using System.Reflection;
-
-namespace Executor
+﻿namespace Executor
 {
     public partial class ThisAddIn
     {
